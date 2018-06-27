303d76617cf088c8d708e2ae17a855bd7b59b8e2
284km <at284km@gmail.com>
2018-06-27 16:29:25 +0900

    Exercise Two: Debugging Exceptions

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -4,4 +4,8 @@ class QuotesController < Rulers::Controller
       "but thinking makes it so. #{[1,2,3].hello}" +
       "\n<pre>\n#{env}\n<pre>"
   end
+
+  def exception
+    raise "It's a bad one!"
+  end
 end
