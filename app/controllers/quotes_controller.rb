43104bec03a878746266e7faf2fde9fe04896a2e
284km <at284km@gmail.com>
2018-06-29 02:30:19 +0900

    Implement quotes#show

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -29,4 +29,10 @@ class QuotesController < Rulers::Controller
     quote_1 = Rulers::Model::FileModel.find(1)
     render :quote, obj: quote_1
   end
+
+  def show
+    quote = FileModel.find(params["id"])
+    ua = request.user_agent
+    render :quote, obj: quote, ua: ua
+  end
 end
