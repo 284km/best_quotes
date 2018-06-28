14c44321552e17eed03668255c92d76c189a2880
284km <at284km@gmail.com>
2018-06-29 01:06:30 +0900

    Implement quotes#new_quote

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -15,6 +15,16 @@ class QuotesController < Rulers::Controller
     render :index, quotes: quotes
   end
 
+  def new_quote
+    attrs = {
+      "submitter" => "web user",
+      "quote" => "A picture is worth one k pixels",
+      "attribution" => "Me"
+    }
+    m = FileModel.create(attrs)
+    render :quote, :obj => m
+  end
+
   def quote_1
     quote_1 = Rulers::Model::FileModel.find(1)
     render :quote, obj: quote_1
